aa6c36693da02de8a431a2fd48df0fbafaeecd4c
sammye234 <tasnimsamiha00@gmail.com>
2026-05-13 12:17:23 +0600

    ..

--- client/src/config/buConfig.js
+++ client/src/config/buConfig.js
@@ -1,10 +1,11 @@
 export const BU_LOGOS = {
   '4AL': '/logos/4AL.png',
-  'GTL': '/logos/GTL.png',
-  'HO': '/logos/HO.png',
-  'BFL': '/logos/BFL.png',
-  'SESL': '/logos/SESL.png',
+  'GTL': '/logos/GTL.jpeg',
+  'HO': '/logos/HO.jpg',
+  'BFL': '/logos/BFL.jpeg',
+  'SESL': '/logos/SESL.jpeg',
 };
+  
 
 export const BU_DISPLAY_LABELS = {
   '4AL': '4AYDL',
